0f33dd0e37ef0421694506739c4ff85773286303
Josefin W <jo2466we-s@student.lu.se>
2022-03-02 18:06:31 +0100

    Random Activity Generator

--- src/app/generator/generator.component.ts
+++ src/app/generator/generator.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Aktiviteter, Aktivitet, SkatteverketService } from '../skatteverket.service';
 
 @Component({
@@ -9,13 +10,38 @@ import { Aktiviteter, Aktivitet, SkatteverketService } from '../skatteverket.ser
 export class GeneratorComponent implements OnInit {
 
   akt: Aktivitet[] = [];
+  activityList: string[] = []; 
+  randomWord: string = '';
   constructor(public skatteverketService: SkatteverketService) { }
 
   ngOnInit(): void {
-    this.skatteverketService.getAktiviteter()
+    /*let test = this.skatteverketService.getAktiviteter()
     .subscribe((data: Aktiviteter) => this.akt = data.results.map(obj => 
-      ({skattefri: (obj as any)['skattefri förmån?'], aktivitet: (obj as any).aktivitet})
-      ))
+      ( {skattefri: (obj as any)['skattefri förmån?'], aktivitet: (obj as any).aktivitet})
+      ))*/
+    this.skatteverketService.getAktiviteter()
+    .subscribe(x => {
+      x.results.map(obj => {
+          if((obj as any)['skattefri förmån?'] === 'Ja' || (obj as any)['skattefri förmån?'] === 'ja' ) {
+            this.activityList.push(obj.aktivitet);
+            //this.randomWord = generateActivity(this.activityList);
+            //
+          }
+        })
+      });
+      console.log('Längd: ' + this.activityList.length);
+      
+  }
+  buttonClick() {
+    this.randomWord = this.generateActivity(this.activityList);
+  }
+  generateActivity(list: string[]) {
+    let random = list.length;
+    let randomIndex = Math.floor(Math.random() * random);
+    return list[randomIndex];
   }
 
 }
+
+ 
+
--- src/app/skatteverket.service.ts
+++ src/app/skatteverket.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
+
 export interface Aktivitet {
   skattefri: string;
   aktivitet: string;
